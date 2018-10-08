dcad9f5e0305bb071995d123dc17ee5fe49ad66c
Justin Huynh <justin@Justins-iMac.local>
2018-10-08 15:00:25 -0400

    Added chapter 1 question 4

--- Chapter_1.py
+++ Chapter_1.py
@@ -70,3 +70,69 @@ def is_perm(s1, s2):
             return False
     return True
 
+# 1.3 URLify: Write a method to replace all spaces in a string with '%20'.
+# You may assume that the string has sufficient space at the end to hold the 
+# additional characters, and that you are given the "true" length of the string. 
+# (Note: if implementing in Java, please use a character array so that you can perform
+# this operation in place.)
+
+# EXAMPLE 
+# Input:  "Mr John Smith      ", 13
+# Output: "Mr%20John%20Smith"
+
+def urlify(s, s_length):
+    # Empty string
+    if s_length == 0:
+        return s
+    return s.replace(" ", "%20")
+
+# 1.4 Palindrome Permutation: Given a string, write a function to check if it is a 
+# permutation of a palindrome. A palindrome is a word or phrase that is the same 
+# forwards and backwards. A permutation is a rearrangement of letters. 
+# The palindrome does not need to be limited to just dictionary words.
+
+# EXAMPLE
+# Input: Tact Coa
+# Output: True (permutations: "taco cat", "atco cta", etc)
+
+def perm_palindrome(s):
+    if len(s) == 0:
+        return True
+    
+    s = s.replace(" ", "")
+    s = s.lower()
+    d = {}
+    for c in s:
+        if c in d:
+            d[c] += 1
+        else:
+            d[c] = 1
+
+    # Check if there is a maximum of 1 odd character count in d
+    count_odd = 0
+    count_even = 0
+    for key, value in d.iteritems():
+        if value % 2 != 0: # odd
+            count_odd += 1
+        else:
+            count_even += 1
+        if count_odd > 1:
+            print(count_odd, count_even)
+            return False
+    return True 
+
+# One Away: There are three types of edits that can be performed on strings: 
+# insert a character, remove a character, or replace a character.
+# Given two strings, write a function to check if they are one edit 
+# (or zero edits) away.
+
+# EXAMPLE
+# pale,  ple  -> true
+# pales, pale -> true
+# pale,  bale -> true
+# pale,  bae  -> true
+
+
+    
+
+
