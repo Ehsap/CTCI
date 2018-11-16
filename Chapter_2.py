586488c968d64a7b8f181151c0e20dd9d87dc6ca
Justin Huynh <polar.truth@gmail.com>
2018-11-16 11:13:46 -0500

    A

--- Chapter_2.py
+++ Chapter_2.py
@@ -12,7 +12,7 @@ class MyLinkedList:
     
     def appendLast(data):
         n = Node(data)
-        if self.head = None:
+        if self.isEmpty()
             self.head = n
             return self.head
         
@@ -21,9 +21,39 @@ class MyLinkedList:
         while (pointer.next != None):
             pointer = pointer.next
         pointer.next = n 
-        return self.head 
-            
+        return self.head
+
+    def isEmpty(self):
+        return self.head == None  
 
 # 2.1 Remove Dups: Write code to remove duplicates from an unsorted linked list
 # Follow up: How would you solve this problem if a temporary bufer isn't allowed?
+    def remove_dups(self):
+        # Dictionary that stores used values 
+        used = {}
+
+        # Go through linked list and add data to the used dictionary
+        n = self.head 
+        while n != None:
+            if n.data not in used:
+                used[n.data] = True
+            else:
+                # Check if last node
+                if n.next = None
+                    n = None 
+                else:
+                # Remove this node in the middle of the list
+                n.next = n.next.next
+            n = n.next 
+        return self.head 
+
+# 2.2 Return Kth to Last: Implement an algorithm to find the kth to last
+# element of a singly linked list
+    def k_to_last(self,k):
+        if self.isEmpty():
+            raise('EmptyLinkedListException')
+        # Go to the end of the list
+
+        # Go back k nodes and return it
+
 
