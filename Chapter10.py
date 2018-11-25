7623a7fb38b6f71bed9ac962d70eb19b1cab169c
Justin Huynh <justin@Justins-iMac.local>
2018-11-25 16:35:43 -0500

    Added 10.3

--- Chapter10.py
+++ Chapter10.py
@@ -68,7 +68,40 @@ def group_anagrams(strings):
         anagrams.append(anagram_group)
     return anagrams
 
+# 10.3 Search in Rotated Array: Given a sorted array of n 
+# integers that has been rotated an uknown number of times,
+# write code to find an element in the array. You may
+# assume that the array was originally sorted in increasing order.
 
+def search_rotated_array(A, k):
+    # Empty Array
+    if len(A) == 0:
+        return -1
+    # Array of Length 1
+    if len(A) == 1:
+        if A[0] == k:
+            return 0
+
+    # Find how many times the array has been shifted by
+    shifts = 0
+    for i in range(len(A) - 1):
+        if A[i + 1] < A[i]:
+            shifts = i
+            break
+    # Binary search through the array using the shifts
+    low = 0
+    mid = (len(A)//2)
+    high = len(A) - 1
+
+    while low < high:
+        mid = (low + high) // 2
+        if k < A[(mid + shifts) % len(A)]:
+            high = mid 
+        elif k > A[(mid + shifts) % len(A)]:
+            low = mid + 1
+        else:
+            return (mid + shifts) % len(A)
+    return -1
 
 class Tests(unittest.TestCase):
 
@@ -82,6 +115,11 @@ class Tests(unittest.TestCase):
         A = [1, None]
         B = [2]
         self.assertEquals(sorted_merge(A,B), [1,2])
+    
+    def test_search_rotated_array(self):
+        A = [15, 16, 19, 20, 25, 1, 3, 4, 5, 7, 10, 14]
+        self.assertEquals(search_rotated_array(A, 5), 8)
+        self.assertEquals(search_rotated_array(A, 21), -1)
     
 if __name__ == '__main__':
     unittest.main()
